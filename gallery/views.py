fe276a4d7ae2df954a18c55f48557d42e8399fc9
gpelayo109 <gpelayo109@gmail.com>
2017-02-20 22:39:50 -0800

    Implemented Gallery for gallery.html
    Other
    -Add Image.dimensions to fix preview scale transition
    -Expanded gallery.html contexts for gallery menu
    -Change color for overlay
    -Moved style.css initialization from base.html to index.html
    -Remove Comments

--- gallery/views.py
+++ gallery/views.py
@@ -6,6 +6,7 @@ class Image:
     description = None
     image_url = None
     thumb_url = None
+    dimensions = None
     tags = None
 
 
@@ -20,6 +21,33 @@ class Gallery:
     def add_image(self, image):
         self.images.append(image)
 
+    def add_image_list(self, image_list):
+        for imge in image_list:
+            self.images.append(imge)
+
+
+dummy_images = []
+
+for i in range(1, 11):
+    im = Image()
+    im.image_url = 'gallery/img/original/{}.jpg'.format(i)
+    im.thumb_url = 'gallery/img/thumbs/{}.jpg'.format(i)
+    im.name = 'Image {}'.format(i)
+    im.dimensions = "800x2000"
+    dummy_images.append(im)
+
+
+gl1 = Gallery()
+gl1.name = "Rooms"
+gl1.url = "Rooms"
+gl1.add_image_list(dummy_images[1:3])
+gl2 = Gallery()
+gl2.name = "Earth"
+gl2.url = "Earth"
+gl2.add_image_list(dummy_images[3:12])
+
+dummy_galleries = {gl1.name: gl1, gl2.name: gl2}
+
 
 class ContextBuilder:
     context = None
@@ -29,24 +57,8 @@ class ContextBuilder:
         self.update()
 
     def update(self):
-        # Get galleries, only name and url
-        im = Image()
-        im.image_url = 'gallery/img/collage/7.jpg'
-        im.thumb_url = 'gallery/img/collage/thumbs/7.jpg'
-        im2 = Image()
-        im2.image_url = 'gallery/img/collage/5.jpg'
-        im2.thumb_url = 'gallery/img/collage/thumbs/5.jpg'
-
-        gl = Gallery()
-        gl.name = "Rooms"
-        gl.url = "test1"
-        gl.add_image(im)
-        gl2 = Gallery()
-        gl2.name = "Earth"
-        gl2.url = "test2"
-        gl2.add_image(im2)
-
-        self.context['gallery_list'] = [gl, gl2]
+        # Note: gallery_list is in every context because of the menu
+        self.context['gallery_list'] = dummy_galleries.values()
 
 
 class IndexContextBuilder(ContextBuilder):
@@ -64,8 +76,12 @@ class GridContextBuilder(ContextBuilder):
 
     def update(self):
         super().update()
+
         if self.gallery_name:
-            self.context['gallery_name'] = self.gallery_name
+            # replace with ORM call for retrieving gallery by name
+            gl = dummy_galleries[self.gallery_name]
+            self.context['gallery_name'] = gl.name
+            self.context['image_list'] = gl.images
 
 
 def index(request):
