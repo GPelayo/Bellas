6f7c92f1605bab66f18d25addef1856a4d2081cc
gpelayo109 <gpelayo109@gmail.com>
2017-04-11 22:35:36 -0700

    Converted to Restful API framework

--- gallery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from gallery import views
-
-urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
-    url(r'^grid/([A-Za-z0-9]+)/$', views.grid, name='grid'),
-    url(r'^$', views.gallery_index_redirect)
-]
