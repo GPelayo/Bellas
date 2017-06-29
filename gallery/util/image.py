deb285ed8cb366a46b652b329d226addf390f638
gpelayo109 <gpelayo109@gmail.com>
2017-06-28 23:08:28 -0700

    Fix Crash when image can't be resized

--- gallery/util/image.py
+++ gallery/util/image.py
@@ -3,8 +3,8 @@ from io import BytesIO
 import imghdr
 import struct
 
-DEFAULT_IMG_WIDTH = 800
-DEFAULT_IMG_HEIGHT = 1200
+DEFAULT_IMG_WIDTH = 1852
+DEFAULT_IMG_HEIGHT = 442
 
 imgerr_log = BellLogger("image_sizer", default_level="INFO")
 
@@ -14,12 +14,12 @@ def calc_image_size(image_buffer, filename="No Name"):
     head = image_buffer.read(24)
     if len(head) != 24:
         imgerr_log.log("Error Short head < 24: {}".format(filename))
-        return
+        return DEFAULT_IMG_WIDTH, DEFAULT_IMG_HEIGHT
     if imghdr.what(image_buffer) == 'png':
         check = struct.unpack('>i', head[4:8])[0]
         if check != 0x0d0a1a0a:
             imgerr_log.log("Error PNG 0x0d0a1a0a: {}".format(filename))
-            return
+            return DEFAULT_IMG_WIDTH, DEFAULT_IMG_HEIGHT
         width, height = struct.unpack('>ii', head[16:24])
     else:
         image_buffer.seek(0)
@@ -42,7 +42,7 @@ def calc_image_size(image_buffer, filename="No Name"):
                 height, width = struct.unpack('>HH', image_buffer.read(4))
             except Exception as e:
                 imgerr_log.log("Error W0703: {}, {}".format(filename, str(e)))
-                return
+                return DEFAULT_IMG_WIDTH, DEFAULT_IMG_HEIGHT
     imgerr_log.log("{}: {}x{}".format(filename, width, height))
 
     width = width if width > 20 else DEFAULT_IMG_WIDTH
